abf2a1430c37b870456b84e161ea546f76e796b4
brazilla-ray <william@mrwilliamwhitaker.com>
2015-12-08 07:43:35 -0500

    Add comments section to all template files.

--- templates/content/node.tpl.php
+++ templates/content/node.tpl.php
@@ -80,6 +80,7 @@
  * @ingroup themeable
  */
 ?>
+
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
