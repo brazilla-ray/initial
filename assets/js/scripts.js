4586883737457afed85b1f598b5df33a79d7076a
brazilla-ray <william@mrwilliamwhitaker.com>
2015-12-08 08:49:11 -0500

    Clean up comments.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,17 @@
-// All the JavaScript for the theme.
+/* All the JavaScript for the theme.
+*/
 
-jQuery(document).ready(function($)	{
-	// For the site navigation.
-	$('#nav-tabs').idTabs("!click");
-	// For slideshows.
-	$('.plates').cycle({
-		slides:'> div',
-		fx:'fade',
-		speed: 1000,
-		timeout: 0,
-		prev:'.prev',
-		next:'.next',
-		swipe:"true",
-		autoHeight:"container",
-		});
+jQuery(document).ready(function($) {
+    $('#nav-tabs').idTabs("!click"); // For the site navigation.
+
+    $('.plates').cycle({ // For slideshows.
+        slides:'> div',
+        fx:'fade',
+        speed: 1000,
+        timeout: 0,
+        prev:'.prev',
+        next:'.next',
+        swipe:"true",
+        autoHeight:"container",
+    });
 });
--- template.php
+++ template.php
@@ -1,27 +1,19 @@
 <?php
-/* Purge needless XHTML stuff. */
-function initial_process_html_tag(&$vars) {
+function initial_process_html_tag(&$vars) { // Purge needless XHTML stuff.
     $el = &$vars['element'];
 
-    /* Remove type="..." and CDATA prefix/suffix. */
-    unset($el['#attributes']['type'], $el['#value_prefix'], $el['#value_suffix']);
+    unset($el['#attributes']['type'], $el['#value_prefix'], $el['#value_suffix']); // Remove type="..." and CDATA prefix/suffix.
 
-    /* Remove media="all" but leave others unaffected. */
     if (isset($el['#attributes']['media']) && $el['#attributes']['media'] === 'all') {
         unset($el['#attributes']['media']);
-    }
+    } // Remove media="all" but leave others unaffected.
 }
 
-/*  Setting updated jquery library on the front-end, leave back-end alone */
-function initial_js_alter(&$javascript) {
-    /* set new path for jquery */
-    $jquery_path = drupal_get_path('theme','initial') . '/assets/js/jquery/jquery-1.10.2.min.js';
-    /* information about jquery from drupal core */
-    $javascript[$jquery_path] = $javascript['misc/jquery.js'];
-    /* information for new version */
-    $javascript[$jquery_path]['version'] = '1.10.2';
-    $javascript[$jquery_path]['data'] = $jquery_path;
-    /* remove core version */
+function initial_js_alter(&$javascript) { // Setting updated jquery library on the front-end, leave back-end alone
+    $jquery_path = drupal_get_path('theme','initial') . '/assets/js/jquery/jquery-1.10.2.min.js'; //  Setting updated jquery library on the front-end, leave back-end alone
+    $javascript[$jquery_path] = $javascript['misc/jquery.js']; // information about jquery from drupal core
+    $javascript[$jquery_path]['version'] = '1.10.2'; // information for new version
+    $javascript[$jquery_path]['data'] = $jquery_path; // remove core version
     unset($javascript['misc/jquery.js']);
 }
 
